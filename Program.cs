227a9f826dfbedd18517ee901fa43d8708f2fde1
alfoae <vzhe.zanjato@gmail.com>
2026-03-27 19:07:39 +0200

    Laboratorna7Task2_1

--- Program.cs
+++ Program.cs
@@ -1,22 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
+
+class BankTerminal
+{
+    // ПУБЛІЧНИЙ делегат (погано)
+    public Action<int> OnMoneyWithdraw;
+
+    public void Withdraw(int amount)
+    {
+        Console.WriteLine($"Зняття {amount} грн");
+
+        // Виклик делегата
+        OnMoneyWithdraw?.Invoke(amount);
+    }
+}
 
 class Program
 {
     static void Main()
     {
-        List<Action> actions = new List<Action>();
+        BankTerminal terminal = new BankTerminal();
 
-        for (int i = 1; i <= 5; i++)
-        {
-            int j = i;
-            actions.Add(() => Console.WriteLine(j));
-        }
+        // Підписка
+        terminal.OnMoneyWithdraw += Log;
 
-        foreach (var action in actions)
-        {
-            action();
-        }
+        // Нормальна робота
+        terminal.Withdraw(100);
+
+        // ❌ "ЗЛОМ" 1 — видаляємо всіх підписників
+        //terminal.OnMoneyWithdraw = null;
+
+        // тепер нічого не викличеться
+        terminal.Withdraw(200);
+
+        // ❌ "ЗЛОМ" 2 — викликаємо вручну БЕЗ операції
+        terminal.OnMoneyWithdraw?.Invoke(9999);
+    }
+
+    static void Log(int amount)
+    {
+        Console.WriteLine($"[LOG] Знято: {amount}");
     }
 }
